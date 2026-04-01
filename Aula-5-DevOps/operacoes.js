d20c05c4be4f7cb21637a53bbc06415b92de13e2
SasKarol <karoline.s.silva@iesb.edu.br>
2026-03-31 21:08:25 -0300

    fix: corrige função de subtração

--- Aula-5-DevOps/operacoes.js
+++ Aula-5-DevOps/operacoes.js
@@ -1,5 +1,5 @@
 function soma(a, b) { return a + b; }
-function subtracao(a, b) { return a + b; }
+function subtracao(a, b) { return a - b; }
 function multiplicacao(a, b) { return a * b; }
 function divisao(a, b) { return a / b; }
 
